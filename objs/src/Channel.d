dbf6a52dabc0af2c3e35bae332141cbb84b1fb14
KarinaLogvina <karinalogvina0@gmail.com>
2024-09-18 21:12:48 +0200

    fix some

--- objs/src/Channel.d
+++
@@ -1,8 +0,0 @@
-objs/src/Channel.o: src/Channel.cpp src/../includes/Channel.hpp \
-  includes/Client.hpp includes/Channel.hpp
-
-src/../includes/Channel.hpp:
-
-includes/Client.hpp:
-
-includes/Channel.hpp:
